e28f443b3506079c347f81dc2869dd143fd90d5c
nanang <nanangaditya2001@gmail.com>
2023-11-05 23:31:07 +0700

    Menambahkan npm tailwindCss

---
+++ clientapp/src/main/java/id/co/mii/clientapp/models/dto/request/RegionRequest.java
@@ -0,0 +1,13 @@
+package id.co.mii.clientapp.models.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegionRequest {
+
+    private String name;
+}
